4ec88ec8715b5241d665b100ad2ab6fe0d52440f
Abhishek.Batham <abhishekbatham2106@gmail.com>
2024-09-23 23:23:08 +0530

    Java learnings

---
+++ src/ex_07062024/Lab003.java
@@ -0,0 +1,17 @@
+package ex_07062024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+            // single comment - This code will not run or executed
+    // 2+2 ?
+        // Purpose of comment
+        // To give instructions to Jr QA or Devs
+        /**
+         * Multiple comments
+         * Documentation purposes
+         *
+         *
+         */
+
+    }
+}
