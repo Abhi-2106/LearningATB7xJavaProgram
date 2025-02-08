57b83f008a2fea6ebee9c30744b800cb92ea0bb2
Abhishek.Batham <abhishekbatham2106@gmail.com>
2025-02-08 19:14:46 +0530

    Java learnings

---
+++ src/Lab055.java
@@ -0,0 +1,9 @@
+public class Lab055 {
+    public static void main(String[] args) {
+    short s =10;
+    char c = 'A';//65 ASCII value
+    int ss = s+c;
+     System.out.println(ss);
+
+    }
+}
---
+++ src/Lab056.java
@@ -0,0 +1,11 @@
+public class Lab056 {
+    public static void main(String[] args) {
+        byte b = 10;
+        int i = b; //implicit typecasting -JVM
+        int b1 = (byte)b; //explicit typecasting -JVM
+
+        int age =65;
+
+        System.out.println(age);
+    }
+}
